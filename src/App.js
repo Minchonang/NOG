a8a1e16f8cd6c79ee5db553547c90d94505e791c
minchonang <green8085@naver.com>
2024-01-29 12:15:25 +0900

    react 세팅 초기화

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Index from "./main/Index";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<Index/>
+		</>
+	);
 }
+
+// 사용한 라이브러리:
+// react-router-dom: v. 6.21.3
 
 export default App;
---
+++ src/main/Index.jsx
@@ -0,0 +1,7 @@
+function Index() {
+   return (
+      <div>자다가도생각이나조</div>
+   )
+}
+
+export default Index;
