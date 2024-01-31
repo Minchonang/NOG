a3d3fd977858ad98db77c7eb481c8ed0b986245a
minchonang <green8085@naver.com>
2024-01-31 15:38:06 +0900

    회원정보수정을 위한 비밀번호 확인 페이지 제작

---
+++ jada_react/src/components/editUserInfo/EditUserInfo.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import common from "../common/css/common.module.css";
+// import style from "./css/FindId.module.css";
+import { API_BASE_URL } from "../../App.js";
+
+function EditUserInfo() {
+	return (
+		<>
+			<div>회원 정보 수정</div>
+		</>
+	);
+}
+
+export default EditUserInfo;
