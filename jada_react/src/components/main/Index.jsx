a460fba3484a9437938415be56585d7c39d6e22b
minchonang <green8085@naver.com>
2024-02-01 15:58:44 +0900

    회원 정보 수정 - 비밀번호 변경 추가

--- jada_react/src/components/main/Index.jsx
+++ jada_react/src/components/main/Index.jsx
@@ -1,3 +1,27 @@
+import { NavLink } from "react-router-dom";
+import common from "../common/css/common.module.css";
+
+function SampleComp() {
+	const go_login = () => {
+		window.location.href = "/login";
+	};
+	const go_userInfo = () => {
+		window.location.href = "/edit_userinfo";
+	};
+	const go_checkUser = () => {
+		window.location.href = "/check_user";
+	};
+	return (
+		<>
+			<div>
+				<button onClick={go_login}>로그인</button>
+				<button onClick={go_checkUser}>본인확인</button>
+				<button onClick={go_userInfo}>회원정보</button>
+			</div>
+		</>
+	);
+}
+
 function Index() {
    return (
       <div>자다가도생각이나조</div>
