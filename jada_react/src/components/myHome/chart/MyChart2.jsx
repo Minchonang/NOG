3bfaed8c96f5bd150d9e71480ca38131e4443385
lim <jjirong2e@naver.com>
2024-02-02 19:35:41 +0900

    차트 분리

--- jada_react/src/components/myHome/chart/MyChart2.jsx
+++ jada_react/src/components/myHome/chart/MyChart2.jsx
@@ -66,76 +66,69 @@ const MyChart = () => {
   return (
     <div className={style.body}>
       <div className={style.container}>
-        <div className={style.main_title}>
+        <div className={style.title}>
           <h1> USER01님의 패턴분석</h1>
-          <span>광주광역시 광산구 </span>
+          <span>광주광역시 광산구</span>
         </div>
 
-        <div className={style.title}>
+        <div className={style.keyword}>
           <h1>120kw </h1>
           <span>이달 사용 전력</span>
+
           <h1>23,500원</h1>
           <span>사용요금</span>
+
           <h1>500원</h1>
           <span>직전달 대비 절약 금액</span>
+        </div>
+        <div className={style.box_container}>
+         <div className={style.chart_box}>
+            <h1 className={style.chart_box_title}>이달 소비 전력량(kW) </h1>          
+
+            <Doughnut data={doughnutData} options={doughnutOptions}  className={style.doughnut_chart}/>
+            <span className={style.doughnut_value}>120kW</span>          
+            <p >이번달 사용량은 120kw 입니다. 이는 매달 평균 사용량 356kw의 56%에 해당합니다. 현재까지의 요금은 약12500원 입니다.</p>          
+            <p>이추이로 소비가 계속 되었을때 이번달 예상 총 사용량은 542kw 요금은 12344원입니다.</p>
+
+
+        </div>
+       
+        
         </div>
         
 
 
-        <div className={style.title}>
+        <div className={style.keyword}>
           <h1> 오후 소비형</h1>
           <span>가장 많은 소비시간대</span>
 
         </div>
-        <div className={style.title}>
+        <div className={style.keyword}>
           <h1>금요일</h1>
           <span>소비량이 가장 많은 요일</span>
           
         </div>
-        <div className={style.title}>
+        <div className={style.keyword}>
           <h1>13일</h1>
           <span>이달 가장 사용량이 많았던 날</span>
         </div>   
 
-        <div className={style.title}>
+        <div className={style.keyword}>
           <h1>4kW</h1>
           <span>하루 평균 사용량</span>
         </div>
-        <div className={style.title}>
+        <div className={style.keyword}>
           <h1>54%</h1>
           <span>지역주민 대비 사용량</span>
         </div>
-        <div className={style.title}>
-          <h1>54%</h1>
-          <span>지역주민 대비 사용량</span>
-        </div>
-        <div className={style.title}>
-          <h1>54%</h1>
-          <span>지역주민 대비 사용량</span>
-        </div>
+       
 
 
 
-      <div className={style.box_container_one}>
-         <div className={style.chart_box}>
-            <span className={style.chart_box_title}>이번달 소비 전력량(kW) </span>          
-
-            <Doughnut data={doughnutData} options={doughnutOptions}  className={style.doughnut_chart}/>
-            
-            <span className={style.doughnut_value}>120kW</span>          
-        </div>
-        {/* <div className={style.chart_box}>
-
-            <span className={style.chart_box_title}>이번달 소비 전력량(kW) </span>          
-
-            <Doughnut data={doughnutData} options={doughnutOptions}  className={style.doughnut_chart}/>
-            <span className={style.doughnut_value}>120kW</span>          
-        </div> */}
-        
-        </div>
+     
 
         {/* 두번째 줄 */}
-      <div className={style.box_container_one}> 
+      <div className={style.box_container}> 
         <div className={style.chart_box}>
           <span className={style.chart_box_title}>소비 유형 </span>          
           <PieChart />
@@ -147,7 +140,7 @@ const MyChart = () => {
         </div> */}
       </div>
 
-      <div className={style.box_container_one}> 
+      <div className={style.box_container}> 
         {/* <div className={style.chart_box}>
           <span className={style.chart_box_title}>소비 유형 </span>          
           <PieChart />
@@ -160,7 +153,7 @@ const MyChart = () => {
       </div>
 
 
-        <div className={style.box_container_one}> 
+        <div className={style.box_container}> 
 
         <div className={style.chart_box}>
           <span className={style.chart_box_title}>요일별 소비 패턴</span>      
@@ -168,7 +161,7 @@ const MyChart = () => {
         </div>
         </div>
 
-        <div className={style.box_container_one}> 
+        <div className={style.box_container}> 
 
 <div className={style.chart_box}>
   <span className={style.chart_box_title}>이달 소비 패턴</span>      
@@ -178,7 +171,7 @@ const MyChart = () => {
 
 
 
-        <div className={style.box_container_one}> 
+        <div className={style.box_container}> 
           <span className={style.chart_box_title}>비교</span>      
 
            
