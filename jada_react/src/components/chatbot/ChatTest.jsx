f9faa63676244b1f38378512c8b1865f84c5f870
bnm6283 <bnm6283@naver.com>
2024-02-02 16:43:48 +0900

    chatbot_update

--- jada_react/src/components/chatbot/ChatTest.jsx
+++ jada_react/src/components/chatbot/ChatTest.jsx
@@ -2,10 +2,14 @@
 import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
 import questionImage from "./nogimg.png";
+import common from "../common/css/common.module.css";
+import style from "./css/ChatTest.module.css";
+import { NavLink } from "react-router-dom";
 
 const socket = io("http://localhost:5000"); // Flask 서버 주소에 맞게 변경
 
 const ChatTest = () => {
+  // const [chatImage, setChatImage] = useState('');
   const [query, setQuery] = useState("");
   const [chatHistory, setChatHistory] = useState([]); // 채팅 기록을 저장할 상태 추가
 
@@ -13,13 +17,12 @@ const ChatTest = () => {
     if (query.trim() !== "") {
       // query 값이 비어있지 않은 경우에만 처리
       socket.emit("message", { Query: query, BotType: "TEST" });
-      setChatHistory([...chatHistory, { question: query, answer: "" }]);
+      setChatHistory([
+        ...chatHistory,
+        { question: query, answer: "", img: "" },
+      ]);
       setQuery(""); // 질문 보낸 후 query 초기화
     }
-
-    // socket.emit('message', { Query: query, BotType: 'TEST' });
-    // // 질문과 대답을 채팅 기록에 추가
-    // setChatHistory([...chatHistory, { question: query, answer: '' }]);
   };
 
   socket.on("message", (data) => {
@@ -29,6 +32,7 @@ const ChatTest = () => {
       updatedHistory[lastIndex] = {
         ...updatedHistory[lastIndex],
         answer: data.Answer,
+        img: data.Img,
       };
       return updatedHistory;
     });
@@ -41,30 +45,31 @@ const ChatTest = () => {
   }, [chatHistory]);
 
   return (
-    <div>
-      <div
-        id="chatContainer"
-        style={{ maxHeight: "700px", overflowY: "auto", marginTop: "1rem" }}
-      >
+    <div className={style.background}>
+      <div className={style.title_area}>
+        <NavLink to="/home">NOG</NavLink>
+        <div>Chatbot</div>
+      </div>
+
+      <div className={style.chatContainer} id="chatContainer">
         <div>
           <img
             src={questionImage}
-            style={{ maxWidth: "50px", height: "50px", marginLeft: "1rem" }}
-          ></img>
-          <p
             style={{
-              backgroundColor: "#FFCF81",
-              padding: "1rem",
-              marginBottom: "2rem",
-              marginLeft: "1rem",
-              marginRight: "10rem",
+              maxWidth: "50px",
+              height: "50px",
+              marginLeft: "1em",
+              marginTop: "4em",
             }}
-          >
+          ></img>
+
+          <div className={style.helloNog}>
             안녕하세요
-            <br /> 저는 챗봇 노그입니다 😊
+            <br /> 저는 챗봇 노지입니다😊
             <br />
             여러분을 돕는 것을 좋아하고
-            <br /> 에너지 낭비를 싫어해요 🥺
+            <br />
+            에너지 낭비를 싫어해요🥺
             <br />
             저와 같이 에너지 절약을 통해
             <br /> 깨끗한 지구를 만들어봐요🌳
@@ -75,24 +80,15 @@ const ChatTest = () => {
             예시1. 넌 누구야?
             <br />
             예시2. 에어컨 온도조절해줘
-          </p>
+          </div>
         </div>
+
         {chatHistory.map((chat, index) => (
           <div key={index}>
-            <div
-              style={{
-                textAlign: "right",
-                backgroundColor: "#F9F7C9",
-                padding: "1rem",
-                marginBottom: "2rem",
-                marginLeft: "10rem",
-                marginRight: "1rem",
-              }}
-            >
-              {chat.question}
-            </div>
+            <div className={style.question_box}>{chat.question}</div>
+
             {chat.answer && (
-              <p style={{ textAlign: "left" }}>
+              <div style={{ textAlign: "left" }}>
                 <img
                   src={questionImage}
                   style={{
@@ -111,29 +107,44 @@ const ChatTest = () => {
                   }}
                 >
                   {chat.answer}
+                  {chat.img && (
+                    <span>
+                      {/(http|https):\/\//.test(chat.img) ? (
+                        <a
+                          href={chat.img}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <img
+                            src={chat.img}
+                            style={{ maxWidth: "100%", height: "100%" }}
+                            alt="chat image"
+                          />
+                        </a>
+                      ) : (
+                        <span>{chat.img}</span>
+                      )}
+                    </span>
+                  )}
                 </p>
-              </p>
+              </div>
             )}
           </div>
         ))}
       </div>
 
-      <div
-        style={{
-          textAlign: "right",
-          padding: "1rem",
-          marginBottom: "2rem",
-          marginLeft: "10rem",
-          marginRight: "1rem",
-        }}
-      >
+      <div className={style.input_area}>
         <input
           type="text"
           value={query}
           onChange={(e) => setQuery(e.target.value)}
         />
-        <button onClick={handleQuery} disabled={!query.trim()}>
-          ⬆️
+        <button
+          className={style.send_btn}
+          onClick={handleQuery}
+          disabled={!query.trim()}
+        >
+          ⭐
         </button>
       </div>
     </div>
