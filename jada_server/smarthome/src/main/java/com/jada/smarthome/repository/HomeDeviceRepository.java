848834863b0ecbd4d75a1153e3f0053a1654ba27
cowhub1 <sowon655@gmail.com>
2024-02-11 21:11:05 +0900

    HomeDeviceRepository수정

--- jada_server/smarthome/src/main/java/com/jada/smarthome/repository/HomeDeviceRepository.java
+++ jada_server/smarthome/src/main/java/com/jada/smarthome/repository/HomeDeviceRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import com.jada.smarthome.model.HomeDevice;
 
 @Repository
-public interface HomeDeviceRepository extends JpaRepository <HomeDevice, String>{
+public interface HomeDeviceRepository extends JpaRepository <HomeDevice, Integer>{
   // HomeDevice 엔터티에서 users 필드를 통해 user_home_id로 HomeDevice를 찾기
   List<HomeDevice> findByHomeId(String homeId);
   List<HomeDevice> findBySerialNum(String serialNum);
