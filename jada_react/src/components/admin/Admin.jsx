724d00cf874acf898acea9cf6a95305a4e0d01ba
bnm6283 <bnm6283@naver.com>
2024-02-13 20:02:50 +0900

    관리자 페이지 관리자만 접근 가능하도록 설정

--- jada_react/src/components/admin/Admin.jsx
+++ jada_react/src/components/admin/Admin.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { API_BASE_URL } from "../../App";
 import Header from "../common/jsx/Header";
 import common from "../common/css/common.module.css";
 import AdminNav from "../common/jsx/AdminNav.jsx";
@@ -9,10 +10,43 @@ import NogiAnalysis from "./NogiAnalysis";
 
 function Admin() {
   const [currentTab, setCurrentTab] = useState("문의사항");
+  const [userRole, setUserRole] = useState("");
+
+  useEffect(() => {
+    getUserRole();
+  }, []);
 
   const handleTabChange = (tab) => {
     setCurrentTab(tab);
   };
+
+  const getUserRole = async () => {
+    const userId = sessionStorage.getItem("user_id");
+    try {
+      const response = await fetch(`${API_BASE_URL}/api/userinfo/getRole`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: userId,
+      });
+
+      if (response.ok) {
+        const result = await response.json();
+        setUserRole(result.role);
+      } else {
+        console.log("서버 오류");
+      }
+    } catch (error) {
+      console.error("서버 통신 오류", error);
+    }
+  };
+
+  if (userRole !== "1") {
+    alert("접근 권한이 없습니다.");
+    window.location.href = "/analysis";
+    return null;
+  }
 
   return (
     <div className={common.background}>
