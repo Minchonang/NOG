919cd667b7d7ea9a146ffa80f81203c5c9ba3ef5
taeyoon <ky1492@gmail.com>
2024-02-15 16:52:10 +0900

    예측작성

--- yolo_pose/flask/coin/upbit_control.py
+++ yolo_pose/flask/coin/upbit_control.py
@@ -32,7 +32,20 @@ class upbit_control:
         now = datetime.datetime.now()
         ago = now - datetime.timedelta(minutes=200)
         return now.strftime('%Y-%m-%dT%H:%M:00'), ago.strftime('%Y-%m-%dT%H:%M:00')
-        
+    def now_and_long_ago(self, long):
+        """
+        200 단위로 넣어야 합니다....
+        :param long:
+        :return:
+        """
+        now = datetime.datetime.now()
+        ran = long / 200
+        rs = []
+        for i in range(int(ran)):
+            temp_ago = now - datetime.timedelta(minutes=200 * i)
+            rs.append( temp_ago.strftime('%Y-%m-%dT%H:%M:00') )
+        return rs
+
     # 업비트 데이터 반환
     def request_upbit_data(self, time_to, count=180, coin_name="KRW-BTC"):
         url = "https://api.upbit.com/v1/candles/minutes/1?market=%s&count=%03d&to=%s" %(coin_name, count, time_to)
@@ -64,10 +77,27 @@ class upbit_control:
 
         rs_400.sort_values(by='candle_date_time_utc',ascending=True, inplace=True)
         return rs_400
-    
+    def request_upbit_long_data(self, ago=1000 ,coin_name="KRW-BTC"):
+        time_str_list = self.now_and_long_ago(ago)
+        count = 200
+        rs_long_df = pd.DataFrame()
+        for time in time_str_list:
+            temp_url = "https://api.upbit.com/v1/candles/minutes/1?market=%s&count=%03d&to=%s" %(coin_name, count, time)
+            headers = {"accept": "application/json", "Accept-Encoding": "gzip"}
+            response = requests.get(temp_url, headers=headers)
+            # response
+            rs = pd.DataFrame()
+            for row in response.json():
+                temp_df = pd.DataFrame([row])
+                rs = pd.concat([rs, temp_df], axis=0 )
+            rs_long_df = pd.concat([rs_long_df, rs], axis=0 )
+        rs_long_df.sort_values(by='candle_date_time_utc',ascending=True, inplace=True)
+        return rs_long_df
+
 
 if __name__ == "__main__":
-    # upbit = upbit_control()
-    # temp = upbit.request_upbit_data( time_to= , count=200 ,coin_name="KRW-XRP" )
-    # print(temp)
-    pass
+    upbit = upbit_control()
+    # temp = upbit.request_upbit_data( time_to= upbit.now(), count=200 ,coin_name="KRW-LSK" )
+    print(upbit.request_upbit_long_data(1000))
+
+    # pass
